e73d87bf444db06dd47640a4cfd9c9638922b067
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-11-28 18:19:22 +0300

    Added a method to get the next piece

--- src/js/Game.js
+++ src/js/Game.js
@@ -15,11 +15,62 @@ export default class Game {
     this.columns = columns;
 
     this.playfield = this.createPlayfield();
+    this.activePiece = this.getNextPiece();
+    this.nextPiece = this.getNextPiece();
   }
 
   createPlayfield() {
-    return playfield = Array(this.rows).fill().map(() =>
-      Array(this.columns).fill(0)
-    );
+    return Array(this.rows).fill().map(() => Array(this.columns).fill(0));
+  }
+
+  getNextPiece() {
+    const type = "IOTSZJL"[Math.floor(Math.random() * 7)];
+    const piece = new Object();
+
+    const pieces = {
+      "I": [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      "O": [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0],
+      ],
+      "T": [
+        [0, 0, 0],
+        [3, 3, 3],
+        [0, 3, 0],
+      ],
+      "S": [
+        [0, 0, 0],
+        [0, 4, 4],
+        [4, 4, 0],
+      ],
+      "Z": [
+        [0, 0, 0],
+        [5, 5, 0],
+        [0, 5, 5],
+      ],
+      "J": [
+        [0, 0, 0],
+        [6, 6, 6],
+        [0, 0, 6],
+      ],
+      "L": [
+        [0, 0, 0],
+        [7, 7, 7],
+        [7, 0, 0],
+      ],
+    };
+
+    piece.x = 0;
+    piece.y = 0;
+    piece.blocks = pieces[type];
+
+    return piece
   }
 }
