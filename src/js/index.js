a8f99b1d14b8b8f593fd773c3bf1ff4a940d6599
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-11-29 00:14:14 +0300

    Added method for rendering the playing field

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import Game from "./Game.js";
+import View from "./View.js";
+
+const root = document.getElementById("root");
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
+window.view = view;
 window.game = game;
