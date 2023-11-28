b15983d6bbc2e293d0a9aaab9fe2c0937a4bb957
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-11-29 01:49:30 +0300

    Finalize Controller class

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -1,1 +1,48 @@
-export default class Controller {}
+export default class Controller {
+  game = null;
+  view = null;
+
+  constructor(game, view) {
+    this.game = game;
+    this.view = view;
+
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+
+  handleKeyDown(event) {
+    if (event.key === "ArrowLeft" || event.key === "h") {
+      game.movePieceLeft();
+      view.render(game.getState());
+
+      return true;
+    }
+
+    if (event.key === "ArrowRight" || event.key === "l") {
+      game.movePieceRight();
+      view.render(game.getState());
+
+      return true;
+    }
+
+    if (event.key === "ArrowUp" || event.key === "k") {
+      game.rotatePiece();
+      view.render(game.getState());
+
+      return true;
+    }
+
+    if (event.key === "ArrowDown" || event.key === "j") {
+      game.movePieceDown();
+      view.render(game.getState());
+
+      return true;
+    }
+
+    if (event.key === " ") {
+      game.dropPiece();
+      view.render(game.getState());
+
+      return true;
+    }
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import Game from "./Game.js";
 import View from "./View.js";
+import Controller from "./Controller.js";
 
 const root = document.getElementById("root");
 
 const game = new Game();
 const view = new View(root, 320, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.view = view;
 window.game = game;
+window.controller = controller;
 
-view.renderPlayfield(game.getState())
+view.renderPlayfield(game.getState());
